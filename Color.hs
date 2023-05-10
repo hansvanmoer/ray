95e2bd3d625d240ebbf0dda5df68957b8c4691a9
hansvanmoer <hansvm.projects@gmail.com>
2023-05-10 22:12:44 +0200

    Added color and material constructors

--- Color.hs
+++ Color.hs
@@ -1,4 +1,17 @@
-module Color (Color) where
+module Color (Color, normalize, rgba) where
 
 -- A color as a RGBA tuple
-type Color = (Float, Float, Float, Float)
+data Color = RGBA Float Float Float Float
+
+-- Creates an RGBA tuple
+rgba :: Float -> Float -> Float -> Float -> Color
+
+rgba r g b a = RGBA (normalize r) (normalize g) (normalize b) (normalize a)
+
+-- Normalizes a channel value
+normalize :: Float -> Float
+
+normalize v
+  | v < 0.0 = 0.0
+  | v > 0.0 = 1.0
+  | otherwise = v
--- Material.hs
+++ Material.hs
@@ -3,3 +3,8 @@ import Color
 
 -- A material with a specified color and reflectivity
 data Material = Colored Color Float
+
+-- Creates a new colored material
+coloredMaterial :: Color -> Float -> Material
+
+coloredMaterial color reflectivity = Colored color (normalize reflectivity)
